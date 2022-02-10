a84fd169fe9a5b2cc6e024b0d015f9061373c853
顾龙雨 <993214226@qq.com>
2022-02-10 12:14:00 +0800

    HelloWorld

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    /* code */
+    cout<<"Hello World!"<<endl;
+    cout<<"name: "<<endl;
+    string name;
+    cin>>name;
+    cout<<name<<endl;
+    return 0;
+}
